817bdb3699a90c8c51ba8452e1c2017b76b0f55d
JI-Maina <irungujmaina@gmail.com>
2023-10-14 21:01:59 +0300

    changed rugby url in usequery

--- src/utilis/FetchRugbyFixtures.jsx
+++ src/utilis/FetchRugbyFixtures.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const FetchRugbyFixtures = async () => {
   const data = await axios.get(
-    "https://apis.tisini.co.ke/apiagent2.php?fixture=all&fixtype=rugby7"
+    "https://apis.tisini.co.ke/apiagent2.php?fixture=all&fixtype=rugby15"
   );
 
   return data;
