fb50f58070c5ece863cf57657ecb071b32d8b225
JI-Maina <irungujmaina@gmail.com>
2023-11-18 10:50:43 +0300

    changed rubgy url2 source

--- src/utilis/FetchRugbyFixtures.jsx
+++ src/utilis/FetchRugbyFixtures.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const FetchRugbyFixtures = async () => {
   const data = await axios.get(
-    "https://apis.tisini.co.ke/apiagent3.php?fixtype=rugby7"
+    "https://apis.tisini.co.ke/apiagent2.php?fixtype=rugby7"
     // "https://apis.tisini.co.ke/apiagent2.php?fixture=all&fixtype=rugby7"
   );
 
